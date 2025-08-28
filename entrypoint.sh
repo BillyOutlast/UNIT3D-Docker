e2e3a0e835be0557b632b259e88fed0df2a0ec13
Shadchamp <172061051+Shadchamp@users.noreply.github.com>
2025-08-28 11:16:57 -0400

    Rebuild alpha 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,14 @@ if [[ "$1" == "setup" ]]; then
     # Run setup commands here
     cd /var/www/html
     composer install --no-interaction --prefer-dist
-    php artisan key:generate --show
+    php artisan key:generate
     echo "Please save this to APP_KEY in UNIT3D/.env"
 fi
 if [[ "$1" == "debug" ]]; then
     echo "Debug mode: keeping container running..."
     tail -f /dev/null
+fi
+if [[ -z "$1" ]]; then
+    echo "Starting Nginx with /etc/nginx/conf.d/unit3d.conf..."
+    nginx -c /etc/nginx/conf.d/unit3d.conf
 fi
